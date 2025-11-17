881b48b90cc37252bda32117343a6bcde0d68221
Kauê Lázaro <lazarok667@gmail.com>
2025-11-17 12:02:00 +0000

    save

---
+++ routers/pessoas.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+import models, schemas
+from database import get_db
+
+router = APIRouter(prefix="/pessoas", tags=["Pessoas"])
+
+@router.post("/", response_model=schemas.Pessoa)
+def criar_pessoa(pessoa: schemas.PessoaCreate, db: Session = Depends(get_db)):
+    nova = models.Pessoa(**pessoa.dict())
+    db.add(nova)
+    db.commit()
+    db.refresh(nova)
+    return nova
+
+@router.get("/", response_model=list[schemas.Pessoa])
+def listar_pessoas(db: Session = Depends(get_db)):
+    return db.query(models.Pessoa).all()
